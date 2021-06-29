3ae15a90065d71d6fea729166efbc18a42895861
Leonardo Burtet <leonardoburtet@outlook.com>
2021-06-28 23:35:36 -0300

    add PHPmailer e sendGrid ; Otimizacao SEO ; Validacao HTML/CSS; Arquivo unico de CSS compress

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: ".formphp", // seletor do formulario
+    button: "#enviar", //seletor do botão
+    erro: "<div id='form-erro><h2>Erro no envio!</h2><p>UM erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // aviso de erro para o usuario
+    sucesso: "<div id='form-sucesso><h2>Formulário enviado com sucessso!</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+  })
+}
